c9580a12139fee05cf205f953b241daf84fc8e6e
mcgtech <mcgonigalstephen@gmail.com>
2020-06-14 10:43:32 +0100

    started coding lastOpened

--- src/utils/date.js
+++ src/utils/date.js
@@ -9,30 +9,47 @@ export function formatDate(date, includeTime)
     return includeTime? date.toString() : date.toDateString()
 }
 
-export function timeSince(date) {
+// https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+// but used - https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site/23259289#23259289
+export var timeSince = function(date) {
+  if (typeof date !== 'object') {
+    date = new Date(date);
+  }
 
   var seconds = Math.floor((new Date() - date) / 1000);
+  var intervalType;
 
   var interval = Math.floor(seconds / 31536000);
+  if (interval >= 1) {
+    intervalType = 'year';
+  } else {
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) {
+      intervalType = 'month';
+    } else {
+      interval = Math.floor(seconds / 86400);
+      if (interval >= 1) {
+        intervalType = 'day';
+      } else {
+        interval = Math.floor(seconds / 3600);
+        if (interval >= 1) {
+          intervalType = "hour";
+        } else {
+          interval = Math.floor(seconds / 60);
+          if (interval >= 1) {
+            intervalType = "minute";
+          } else {
+            interval = seconds;
+            intervalType = "second";
+          }
+        }
+      }
+    }
+  }
 
-  if (interval > 1) {
-    return interval + " years";
+  if (interval > 1 || interval === 0) {
+    intervalType += 's';
   }
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
-    return interval + " months";
-  }
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
-    return interval + " days";
-  }
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
-    return interval + " hours";
-  }
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) {
-    return interval + " minutes";
-  }
-  return Math.floor(seconds) + " seconds";
-}
+
+  return interval + ' ' + intervalType;
+};
